c0b31a589edef2b93f892dde5f7e0ae62960ba69
Noah Bass <bassnh@mail.uc.edu>
2019-12-08 01:10:44 -0500

    Add sorted method for data

--- front-end-visualization/src/charts/SubredditChart.jsx
+++ front-end-visualization/src/charts/SubredditChart.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import Highcharts from 'highcharts/highstock'
 import HighchartsReact from 'highcharts-react-official'
 
+// TODO: read in CSV data into this data structure 
+const data = [
+    { 'sub': '/r/soccer', 'value': -0.1 },
+    { 'sub': '/r/gaming', 'value': 0.432 },
+    { 'sub': '/r/AskReddit', 'value': -0.1843 },
+    { 'sub': '/r/baseball', 'value': -0.326 },
+    { 'sub': '/r/uCinci', 'value': -0.048 },
+    { 'sub': '/r/cincinnati', 'value': 0.013 },
+    { 'sub': '/r/trees', 'value': 0.3 },
+    { 'sub': '/r/funny', 'value': -0.4 }
+]
+
+// Create sorted data (high to low)
+const sortedData = data.sort((a, b) => (a['value'] < b['value']) ? 1 : ((b['value'] < a['value']) ? -1 : 0))
+const categories = sortedData.map(entry => entry['sub'])
+const values = sortedData.map(entry => entry['value'])
+
 const options = {
     chart: {
         type: 'column',
@@ -13,23 +30,23 @@ const options = {
     },
     xAxis: {
     	// list of subreddits here
-        categories: ['/r/soccer', '/r/gaming', '/r/AskReddit', '/r/baseball', '/r/uCinci', '/r/cincinnati', '/r/trees', '/r/funny', '/r/pics'],
+        categories: categories,
         title: {
         	text: 'Subreddits'
         }
     },
     yAxis: {
-    	max: 1,
-      min: -1,
-      title: {
-      	text: 'Sentiment Value (-1 is most negative, 1 is most positive)'
-      }
+        max: 1,
+        min: -1,
+        title: {
+      	    text: 'Sentiment Value (-1 is most negative, 1 is most positive)'
+        }
     },
     credits: {
         enabled: false
     },
     series: [{
-        data: [-0.1, 0.432, -0.1843, -0.326, -0.048, 0.013, 0.3, 0.3, -0.4]
+        data: values
     }]
 }
 
